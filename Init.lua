33b51aa9db79f65fcebe097a1dfadf21f9dda2fa
Real-Aya <realaya@realaya.xyz>
2025-06-09 22:08:32 +0700

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,13 +1,13 @@
 
 if _G.ScriptUrl then
-    return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/q5atznpu7i1crz0bvf58reaswdu8mj6p.lua"))()
+    return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/loaders/q5atznpu7i1crz0bvf58reaswdu8mj6p.lua"))()
 end
 local game_map = {
     [7436755782] = function() -- setclipboard(game.GameId) this how to get game id
         if getgenv().ConfigsReroll then
-            return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/ej5yimioj491bp6wvjfijccqq1vz18sa.lua"))()
+            return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/loaders/ej5yimioj491bp6wvjfijccqq1vz18sa.lua"))()
         end
-        return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/98vt13x1h979yxngcvxbkjsq8hsw9rpw.lua"))()
+        return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/loaders/98vt13x1h979yxngcvxbkjsq8hsw9rpw.lua"))()
     end,
 }
 
