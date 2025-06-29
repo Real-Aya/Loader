369f54de7af28884918c23daac5db1e5c8562902
Real-Aya <realaya@realaya.xyz>
2025-06-30 00:25:26 +0700

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,3 +1,3 @@
 if ConfigsKaitun and not ConfigsKaitun["Lock Pets"] then 
-	return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/98vt13x1h979yxngcvxbkjsq8hsw9rpw.lua"))()
+	return loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/loaders/98vt13x1h979yxngcvxbkjsq8hsw9rpw.lua"))()
 end
